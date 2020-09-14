567aeb13f7095663c8deaa221ee7aa483b7ac648
nomisrevol <vdn1999bxvp@gmail.com>
2020-09-14 21:58:40 +0700

    Add hardcode database

--- app.py
+++ app.py
@@ -1,6 +1,43 @@
-from flask import Flask
+import json
+
+from flask import Flask, jsonify
 
 app = Flask(__name__)
+
+
+@app.route('/hotdeal')
+def hotdeal_handler():
+    with open('./data/hotdeal.json') as json_file:
+        data = json.load(json_file)
+        return jsonify(data)
+
+
+@app.route('/product', defaults={'product_id': None})
+@app.route('/product/<product_id>', strict_slashes=False)
+def get_product(product_id):
+    with open("./data/product.json") as json_file:
+        data = json.load(json_file)
+        return jsonify(data)
+
+
+@app.route('/category')
+def get_category():
+    with open('./data/category.json') as json_file:
+        data = json.load(json_file)
+        return jsonify(data)
+
+
+@app.route('/intro')
+def get_intro_data():
+    with open('./data/intro.json') as json_file:
+        data = json.load(json_file)
+        return jsonify(data)
+
+
+@app.route('/sample-image')
+def get_sample_image():
+    return jsonify(
+        {'image_url': "https://res.cloudinary.com/vdn1999bxvp/image/upload/v1600095144/intro-home_ua2g6v.png"})
 
 
 @app.route('/')
