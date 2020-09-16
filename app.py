fee695d3eb2354881f50313a9604e09a27bc1fdd
nomisrevol <vdn1999bxvp@gmail.com>
2020-09-16 15:45:36 +0700

    Hack for dataset

--- app.py
+++ app.py
@@ -1,35 +1,48 @@
 import json
 
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
+from flask_pymongo import PyMongo
 
 app = Flask(__name__)
-
+app.config["MONGO_URI"] = "mongodb://localhost:27017/teko"
+mongo = PyMongo(app)
+ALL_DATA = []
+hash_sku = {}
 
 @app.route('/hotdeal')
 def hotdeal_handler():
-    with open('./data/hotdeal.json') as json_file:
+    with open('data/hotdeal.json') as json_file:
         data = json.load(json_file)
         return jsonify(data)
 
 
-@app.route('/product', defaults={'product_id': None})
-@app.route('/product/<product_id>', strict_slashes=False)
-def get_product(product_id):
-    with open("./data/product.json") as json_file:
-        data = json.load(json_file)
-        return jsonify(data)
+@app.route('/product/<product_sku>', strict_slashes=False)
+def get_product(product_sku):
+    return jsonify(ALL_DATA[hash_sku[str(product_sku)]])
 
 
 @app.route('/category')
 def get_category():
-    with open('./data/category.json') as json_file:
+    with open('data/category.json') as json_file:
         data = json.load(json_file)
         return jsonify(data)
 
 
+@app.route('/product-range')
+def get_product_range():
+    start = request.args.get('from', default=0, type=int)
+    offset = request.args.get('offset', default=1, type=int)
+    return jsonify(ALL_DATA[start:(start + offset)])
+
+
+@app.route('/all-product')
+def get_all_product():
+    return jsonify(ALL_DATA)
+
+
 @app.route('/intro')
 def get_intro_data():
-    with open('./data/intro.json') as json_file:
+    with open('data/intro.json') as json_file:
         data = json.load(json_file)
         return jsonify(data)
 
@@ -44,6 +57,21 @@ def get_sample_image():
 def hello_world():
     return 'Hello World!'
 
+
+def initialize_data():
+    with open('./data/all.json', "r") as fp:
+        counter = 0
+        for line in fp:
+            json_data = json.loads(line)
+            ALL_DATA.append(json_data)
+            existing_document = mongo.db.teko.find_one(json_data)
+            if not existing_document:
+                mongo.db.teko.insert(json_data, check_keys=False)
+            hash_sku[json_data["sku"]] = counter
+            counter += 1
+
+
+initialize_data()
 
 if __name__ == '__main__':
     app.run()
